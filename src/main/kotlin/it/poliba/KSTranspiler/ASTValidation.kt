c369446bff838b07aa14ea5a30d895d2af3e05df
Paolo Rotolo <paolo@rotolo.dev>
2023-01-18 01:54:08 +0100

    Validate variables and add tests

--- src/main/kotlin/it/poliba/KSTranspiler/ASTValidation.kt
+++ src/main/kotlin/it/poliba/KSTranspiler/ASTValidation.kt
@@ -1,12 +1,41 @@
 package it.poliba.KSTranspiler
 
 import com.strumenta.kolasu.model.Node
+import com.strumenta.kolasu.model.children
+import com.strumenta.kolasu.traversing.findAncestorOfType
+import com.strumenta.kolasu.traversing.walk
+import com.strumenta.kolasu.traversing.walkAncestors
+import it.poliba.KSTranspiler.SwiftParser.VarDeclarationContext
 import java.util.*
 
 fun Node.commonValidation(): LinkedList<Error> {
     val errors = LinkedList<Error>()
 
-    // check a variable is not duplicated
+    this.specificProcess(ControlStructureBody::class.java) {
+        val varsByName = HashMap<String, PropertyDeclaration>()
+
+        if (it is Block) {
+            it.body
+                .filterIsInstance<PropertyDeclaration>()
+                .forEach {
+                    if (varsByName.containsKey(it.varName)) {
+                        it.walkAncestors().forEach {
+                            println("test ${it.nodeType}")
+                        }
+                        errors.add(
+                            Error(
+                                "A variable named '${it.varName}' has been already declared at ${varsByName[it.varName]?.position?.start}",
+                                it.position?.start?.asPosition
+                            )
+                        )
+                    } else {
+                        varsByName[it.varName] = it
+                    }
+                }
+        }
+    }
+
+    // check for list correct types
     this.specificProcess(ListExpression::class.java) {
         val listType = it.itemsType
         val itemTypes = it.items.map { it.type }
@@ -16,7 +45,7 @@ fun Node.commonValidation(): LinkedList<Error> {
         if (differentTypes.isNotEmpty()){
             errors.add(Error("""
                     List can't contain different types.
-                    Found ${differentTypes.first()} in a list of $listType
+                    Found ${differentTypes.first().nodeType} in a list of ${listType.nodeType}
                     """.trimIndent(), this.position))
         }
     }
--- src/main/kotlin/it/poliba/KSTranspiler/Main.kt
+++ src/main/kotlin/it/poliba/KSTranspiler/Main.kt
@@ -7,7 +7,15 @@ import it.poliba.KSTranspiler.facade.KotlinParserFacadeScript
 object App {
     @JvmStatic
     fun main(args: Array<String>) {
-        var code = """val test = listOf<Int>(1, 2, "a")""".trim()
+        var code = """
+            val a = 1
+            
+            fun test() {
+                val b = 2
+                val a = 2
+            }
+            
+        """.trimIndent().trim()
         var code2 = "let a = 5 + 2"
         var actual = code
         val parseResult = KotlinParserFacade.parse(actual)
