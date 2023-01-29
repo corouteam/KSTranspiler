8a52515eafe385255677aae4b5cefb65962f0ab4
francescopaolodellaquila <paolodellaquila1999@gmail.com>
2023-01-29 13:12:26 +0100

    update lexer; map parser + test

--- src/test/kotlin/SWIFTLexerTest.kt
+++ src/test/kotlin/SWIFTLexerTest.kt
@@ -110,7 +110,7 @@ class SWIFTLexerTest {
     }
 
 
-    
+
     @Test
     fun parseVStack(){
         val code = "VStack(alignment: VerticalAlignment.leading, spacing: 10)"
@@ -131,7 +131,7 @@ class SWIFTLexerTest {
         )
         assertEquals(result, tokens(lexerForCode(code)))
     }
-    
+
     @Test
     fun parseSpacer(){
         val code = "Spacer()"
@@ -153,7 +153,7 @@ class SWIFTLexerTest {
         val result = listOf("SPACER_WIDGET","LPAREN","RPAREN", "DOT", "FRAME", "LPAREN", "WIDTH", "COLON", "DOUBLE_LIT", "COMMA", "HEIGHT", "COLON", "DOUBLE_LIT", "RPAREN", "EOF")
         assertEquals(result, tokens(lexerForCode(code)))
     }
-    
+
     @Test
     fun parseCGFloat(){
         val code = "let margin = CGFloat(8)"
